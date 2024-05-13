9bbb2f2904c36e045e1b815c07f215a0ee9bd434
frangomezvenegas <info.fran.gomez@gmail.com>
2024-05-13 23:34:45 +0200

    files for new instances deployment

--- publishWithInstall - com.sh
+++ publishWithInstall - com.sh
@@ -4,7 +4,7 @@
 sh ./install.sh
 
 #demo platform instance
-bash deploy.2demoV0.9.2.prod.sh
-bash deploy.2demoV0.9.2.testing.sh
+bash deploy.2demoV0.9.2com.prod.sh
+bash deploy.2demoV0.9.2com.testing.sh
 
 
